d22f3544fc18b701c94c6f68502240a0ea83ba4d
Allan Patrick <allanpatrick18@gmail.com>
2021-12-21 15:30:31 -0300

    feat:adding triggers for events table adding constrain not null for boolean fields

--- app.py
+++ app.py
@@ -1,16 +1,62 @@
 import uvicorn
+from typing import Optional
 from typing import List
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-app = FastAPI()
+from datetime import datetime
+
+# from create_tables import main
+# main()
+
 import model
 from sports_book.view import *
+import logging
+
+
+#APScheduler Related Libraries
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+
+app = FastAPI()
+
+Schedule = None
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 
 
 @app.get("/")
 async def read_main():
     return {"msg": "Sever Up!"}
 
+
+@app.on_event("startup")
+async def load_schedule_or_create_blank():
+    """
+    Instatialise the Schedule Object as a Global Param and also load existing Schedules from Postgres
+    This allows for persistent schedules across server restarts.
+    """
+    global Schedule
+    try:
+        jobstores = {
+            'default': SQLAlchemyJobStore(url='postgresql://postgres:postgres@localhost:5432/sports_book')
+        }
+        Schedule = AsyncIOScheduler(jobstores=jobstores)
+        Schedule.start()
+        logger.info("Created Schedule Object")
+    except:
+        logger.error("Unable to Create Schedule Object")
+
+
+@app.on_event("shutdown")
+async def pickle_schedule():
+    """
+    An Attempt at Shutting down the schedule to avoid orphan jobs
+    """
+    global Schedule
+    Schedule.shutdown()
+    logger.info("Disabled Schedule")
 
 """
 Sports API
@@ -27,12 +73,27 @@ async def read_all_sports():
     return get_all_objects('sports')
 
 
+@app.get("/sports/", response_model=List[model.Sports])
+async def read_all_sports(q: Optional[str] = None):
+    if q:
+        print(q)
+    return get_all_objects('sports')
+
+
 @app.put("/sports/{sport_id}", response_model=model.Sports)
 async def update_sport(sport_id: str, item: model.Sports):
     update_item_encoded = jsonable_encoder(item)
     sport_obj = Sports.parse_obj(update_item_encoded)
-    create_update_sports(sport_obj, sport_id)
-    return update_item_encoded
+    result = create_update_sports(sport_obj, sport_id)
+    return result
+
+
+@app.post("/sports/", response_model=model.Sports)
+async def update_sport(sport: model.Sports):
+    update_item_encoded = jsonable_encoder(sport)
+    sport_obj = Sports.parse_obj(update_item_encoded)
+    result = create_update_sports(sport_obj)
+    return result
 
 
 @app.post("/sports/", response_model=model.Sports)
@@ -57,11 +118,23 @@ async def read_all_events():
     return get_all_objects('events')
 
 
+def check_schedule_event(event):
+    """ This event"""
+    end_date = event['scheduled_start']
+    time_to_expiry = (end_date - datetime.now()).days
+    print(time_to_expiry)
+
+
 @app.put("/events/{event_id}", response_model=model.Events)
 async def update_events(event_id: int, item: model.Events):
     update_item_encoded = jsonable_encoder(item)
     event = model.Events.parse_obj(update_item_encoded)
     event = create_update_event(event, event_id)
+    # end_date = event['scheduled_start']
+    # time_to_expiry = (end_date - datetime.now()).days
+    # schedule_check = Schedule.add_job(check_schedule_event, 'interval', seconds=time_to_expiry, id=event['id'],
+    #                                   args=[event])
+    # print(schedule_check)
     return event
 
 
@@ -70,12 +143,19 @@ async def create_events(item: model.Events):
     update_item_encoded = jsonable_encoder(item)
     event = model.Events.parse_obj(update_item_encoded)
     event = create_update_event(event)
+    end_date = event['scheduled_start']
+    index_job = str(event['id']) + '_' + event['name']
+    # time_to_expiry = (end_date - datetime.now()).days
+    # schedule_check = Schedule.add_job(check_schedule_event, 'interval', seconds=abs(time_to_expiry),
+    #                                   id=index_job, args=[event])
+    # print(schedule_check)
     return event
 
 
 """
 Selections API
 """
+
 
 @app.get("/selections/{selection_id}", response_model=model.Selections)
 async def read_selections(selection_id: int):
